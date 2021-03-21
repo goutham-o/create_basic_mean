510bf270851ba871bec1f72f7c9a0b3b9c9ad30b
goutham-o <studygouthamm@gmail.com>
2021-03-21 23:06:20 +0530

    Initial Setup

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from "rxjs";
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss'],
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postSub: Subscription;
+
+  constructor(private postService: PostService) {}
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+    this.postSub = this.postService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
+  }
+}
